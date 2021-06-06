6adb76d0498ab0665267148685cb57effd9fc24b
dglebowicz <80824630+dglebowicz@users.noreply.github.com>
2021-06-06 06:58:57 +0200

    Update forms.py

--- Flask app/forms.py
+++ Flask app/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, IntegerField, SubmitField
 
 
 class SimForm(FlaskForm):
-    points = IntegerField(label='Number of points except depot (even, max 50)')
-    number_vehicles = IntegerField(label='Number of vehicles (recommended between 2 and 7)')
-    capacity = IntegerField(label='Capacity of each vehicle')
+    points = IntegerField(label='Number of points except depot (even, max 50)',validators=[DataRequired()])
+    number_vehicles = IntegerField(label='Number of vehicles (recommended between 2 and 7)',validators=[DataRequired()])
+    capacity = IntegerField(label='Capacity of each vehicle',validators=[DataRequired()])
+    tlimit = IntegerField(label='Solution search time limit (seconds)',validators=[DataRequired()])
     submit = SubmitField(label='Start')
